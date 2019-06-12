9af90c5a485d390be9181a550df9d43ded1ac1b4
Tiantian Han <than2@andrew.ad.cmu.edu>
2019-06-12 13:42:11 -0400

    Add manual backup tool

---
+++ manualBackup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+""" 
+manualBackup.py : manually make a backup
+
+Motivation: incremental save creates too many files with trivial changes.
+Where manual backup is preferred, this script makes it easier to create and save a backup 
+copy of the current scene
+"""
+
+import maya.cmds as cmds
+
+workspaceDir = cmds.workspace( q=True, dir=True )
+
+saveDir = cmds.fileDialog2(ff="*.mb", dialogStyle=1)[0]
+thisDir = cmds.file(q=True, expandName=True)
+print ("Saving backup to " + saveDir)
+
+cmds.file(rn=saveDir)
+cmds.file(save=True)
+cmds.file(rn=thisDir)
+
+print ("Done manual backup.")
